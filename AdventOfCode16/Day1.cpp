c0d9382d8337d26e3bd97760211733d96e6a7311
Sven Harenburg <sven@svenharenburg.de>
2021-08-10 13:31:04 +0200

    Day1 Part1

---
+++ AdventOfCode16/Day1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#include "Day1.h"
+
+namespace day1 {
+
+	int solve(std::string input)
+	{
+		std::cout << "Starting to solve Day1\n";		
+		int floor{ 0 };
+
+		for (char& c : input) {
+			if (c == '(') {
+				floor++;
+			}
+			else if (c == ')') {
+				floor--;
+			}
+			else {
+				std::cout << "Invalid character: " << c;
+			}
+		}
+
+
+
+		return floor;
+	}
+}
---
+++ AdventOfCode16/Day1.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+
+namespace day1 {
+	int solve(std::string input);
+}
